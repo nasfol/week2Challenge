10802224e30024791ac21222da5bb1b5c8ab9fc4
Foluso Adegboye <adegboyefoluso@yahoo.com>
2020-11-19 13:22:01 -0500

    first commit

---
+++ Greeter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterApp
+{
+     public class Greeter
+    {
+        
+        public void HelloExample(string name)
+        {
+         
+            Console.WriteLine($"Hello {name}.");
+            
+
+        }
+
+
+        
+        public string Farewell(string name)
+        {
+            
+            Console.WriteLine($"Goodbye {name}.");
+            return name;
+        }
+        //Build a method that says Good Morning/Afternoon/Evening/Night depending on the time of day.
+        public void TimeofDay(string name)
+
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine($"Good Morning {name} ");
+
+            }
+            else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour <= 16)
+            {
+                Console.WriteLine($"Good Afternoon {name}");
+
+            }
+            else if (DateTime.Now.Hour > 16 && DateTime.Now.Hour <= 20)
+            {
+                Console.WriteLine($"Good evening {name}");
+            }
+            else
+            {
+                Console.WriteLine($"GoodNight {name}");
+            }
+
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Greeter greeter = new Greeter();
+
+
+            greeter.HelloExample(name);
+            greeter.Farewell(name);
+            greeter.TimeofDay(name);
+            Console.ReadLine();
+
+
+
+            Greeter love = new Greeter();
+
+            love.Farewell(name);
+            love.TimeofDay(name);
+            love.HelloExample(name);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
